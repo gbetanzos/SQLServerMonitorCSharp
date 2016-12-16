7c5736f9be5c7d216f5ad57b74ad22fa28d7a530
gbetanzos <betanzos@gmail.com>
2016-12-16 17:50:54 -0600

    Initial commit

---
+++ wfaMonitorSQLUsage/frmMonitorSQLUSage.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace wfaMonitorSQLUsage
+{
+    partial class frmMonitorSQLUSage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.msMenu = new System.Windows.Forms.MenuStrip();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtStringConn = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.nUD = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsUser = new System.Windows.Forms.ToolStripStatusLabel();
+            this.msMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nUD)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // msMenu
+            // 
+            this.msMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.msMenu.Location = new System.Drawing.Point(0, 0);
+            this.msMenu.Name = "msMenu";
+            this.msMenu.Size = new System.Drawing.Size(703, 24);
+            this.msMenu.TabIndex = 0;
+            this.msMenu.Text = "menuStrip1";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // runToolStripMenuItem
+            // 
+            this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.runToolStripMenuItem.Text = "&Run";
+            this.runToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Enabled = false;
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.stopToolStripMenuItem.Text = "&Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // txtStringConn
+            // 
+            this.txtStringConn.Location = new System.Drawing.Point(109, 27);
+            this.txtStringConn.Name = "txtStringConn";
+            this.txtStringConn.Size = new System.Drawing.Size(582, 20);
+            this.txtStringConn.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "String Connection";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 80);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtLog.Size = new System.Drawing.Size(679, 197);
+            this.txtLog.TabIndex = 3;
+            // 
+            // nUD
+            // 
+            this.nUD.Location = new System.Drawing.Point(109, 54);
+            this.nUD.Name = "nUD";
+            this.nUD.Size = new System.Drawing.Size(65, 20);
+            this.nUD.TabIndex = 4;
+            this.nUD.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Minutes";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsStatus,
+            this.tsUser});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 282);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(703, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsStatus
+            // 
+            this.tsStatus.Name = "tsStatus";
+            this.tsStatus.Size = new System.Drawing.Size(19, 17);
+            this.tsStatus.Text = "...";
+            // 
+            // tsUser
+            // 
+            this.tsUser.Name = "tsUser";
+            this.tsUser.Size = new System.Drawing.Size(19, 17);
+            this.tsUser.Text = "...";
+            // 
+            // frmMonitorSQLUSage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 304);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nUD);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStringConn);
+            this.Controls.Add(this.msMenu);
+            this.MainMenuStrip = this.msMenu;
+            this.Name = "frmMonitorSQLUSage";
+            this.Text = "Monitor SQL Usage";
+            this.Load += new System.EventHandler(this.frmMonitorSQLUSage_Load);
+            this.msMenu.ResumeLayout(false);
+            this.msMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nUD)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip msMenu;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtStringConn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.NumericUpDown nUD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tsStatus;
+        private System.Windows.Forms.ToolStripStatusLabel tsUser;
+    }
+}
+
---
+++ wfaMonitorSQLUsage/frmMonitorSQLUSage.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Timers;
+using System.IO;
+
+namespace wfaMonitorSQLUsage
+{
+    public partial class frmMonitorSQLUSage : Form
+    {
+        int minutes=15;
+        SqlConnection conn;
+        System.Timers.Timer aTimer;
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            checkSQLUsage();
+        }
+
+        private void checkSQLUsage() {
+            StringBuilder sb = new StringBuilder();
+            this.conn = new SqlConnection(this.txtStringConn.Text);
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+            string path = Application.StartupPath.ToString();
+            string filename = DateTime.Now.ToString()+".csv";
+            filename=filename.Replace("/","_");
+            filename = filename.Replace(":", "_");
+            filename = filename.Replace(" ", "_");
+            //MessageBox.Show(path);
+            StreamWriter sw = new StreamWriter(path+ "\\" + filename);
+            this.log("File to be generated under " + path + "\\" + filename);
+            string [] columnNames;
+            cmd.CommandText = "sp_who";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = conn;
+
+            conn.Open();
+
+            reader = cmd.ExecuteReader();
+            
+            columnNames=new string[(int)reader.FieldCount];
+
+            for(int i=0; i<reader.FieldCount;i++){
+                columnNames[i] = (string)reader.GetName(i); 
+            }
+
+            //Create headers
+            sb.Append(string.Join(",", columnNames));
+
+            //Append Line
+            sb.AppendLine();
+            while (reader.Read()) {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    string value = reader[i].ToString();
+                    if (value.Contains(","))
+                        value = "\"" + value + "\"";
+
+                    
+                    sb.Append(value.Replace(Environment.NewLine, " ").Trim() + ",");
+                }
+                sb.Length--; // Remove the last comma
+                sb.AppendLine();
+            }
+            conn.Close();
+            sw.Write(sb.ToString());
+            sw.Close();
+            this.log("File generated");
+        }
+        public frmMonitorSQLUSage()
+        {
+            InitializeComponent();
+        }
+        
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            stopToolStripMenuItem.Enabled = true;
+            runToolStripMenuItem.Enabled = false;
+            exitToolStripMenuItem.Enabled = false;
+            
+            this.minutes = (int)this.nUD.Value;
+            this.tsStatus.Text = "Running...";
+            this.log("Running... every "+this.minutes.ToString()+ " minute(s)");
+            try
+            {
+                this.run();
+            }
+            catch (Exception ex) {
+                aTimer.Stop();
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void run()
+        {
+            aTimer = new System.Timers.Timer();
+            this.minutes = (int)this.nUD.Value;
+
+            // Hook up the Elapsed event for the timer.
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+
+            // Set the Interval to 2 seconds (2000 milliseconds).
+            aTimer.Interval = (1000 * 60 * this.minutes);
+            aTimer.Enabled = true;
+
+        }
+
+        private void frmMonitorSQLUSage_Load(object sender, EventArgs e)
+        {
+			txtStringConn.Text = "Server=SQLServer;Database=master;Trusted_Connection=True;";
+            tsUser.Text = Environment.UserDomainName.ToString() + "\\" + Environment.UserName ;
+        }
+
+        private void saveConfigurationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            aTimer.Stop();
+            tsStatus.Text = "Stopped";
+            stopToolStripMenuItem.Enabled = false;
+            runToolStripMenuItem.Enabled = true;
+            exitToolStripMenuItem.Enabled = true;
+        }
+
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void log(string value) {
+            
+            if (this.txtLog.InvokeRequired)
+            {
+                // It's on a different thread, so use Invoke.
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke
+                    (d, new object[] { value + " (Invoke)" });
+            }
+            else
+            {
+                // It's on the same thread, no need for Invoke 
+                //this.textBox1.Text = text + " (No Invoke)";
+                txtLog.Text += DateTime.Now.ToString() + " - " + value + Environment.NewLine;
+            }
+        }
+
+        // This delegate enables asynchronous calls for setting
+        // the text property on a TextBox control.
+        delegate void SetTextCallback(string value);
+
+        // This method is passed in to the SetTextCallBack delegate 
+        // to set the Text property of textBox1. 
+        private void SetText(string value)
+        {
+            txtLog.Text += DateTime.Now.ToString() + " - " + value + Environment.NewLine;
+        }
+    }
+}
